ab8b0ffd799882b84ea0b20054ff508e7c142646
zhiyang.zzy <993001803@qq.com>
2020-07-27 23:43:53 +0800

    add back translate

--- back_translate.py
+++ back_translate.py
@@ -57,18 +57,29 @@ def baidu_translate(ori_query: str, toLang='zh', fromLang='auto'):
             httpClient.close()
     return query_arr
 
-
-if __name__ == '__main__':
-    queries = '附近有什么好玩的'.split(",")
-    # 根据语言列表，可以翻译成多个句子, language: en,jp,kor,fra,spa,th,ara,ru,pt,de,it,el,nl,pl,bul,est,dan,fin,cs,rom,slo,swe,hu,cht,vie...
-    for query in queries:
-        out_arr = []
+def back_translate(query_arr):
+    out_arr = {}
+    for query in query_arr:
+        cur_arr = []
+        # 根据语言列表，可以翻译成多个句子, language: en,jp,kor,fra,spa,th,ara,ru,pt,de,it,el,nl,pl,bul,est,dan,fin,cs,rom,slo,swe,hu,cht,vie...
         lan_list = "en,jp,kor".split(",")
         for tmp_lan in lan_list:
             for tmp_q in baidu_translate(query, tmp_lan):
-                out_arr.extend(baidu_translate(tmp_q, 'zh'))
-        print(list(set(out_arr)))
-    # ['帮我查一下航班信息', '请帮我查一下飞机的情报。', '帮我检查航班信息。', '检查我的航班信息。', '检查航班', '查一下我的飞行记录。', '查一下我的航班信息。', '检查一下飞行资料', '检查飞行数据。', '帮我查一下航班信息。', '帮我查一下VOO信息', '帮我查一下航班数据。', '帮我查一下飞行记录。', '请查一下飞机的信息。', '幫我查一下班機資訊']
-    # ['打听一下航班的信息。', '检查航班', '检查VOO信息', '查看航班信息', '检查飞行数据。', '四航班检查', '检查航班信息。', '航班信息验证', '查一下班機資訊', '请查一下飞机的信息。', '检查飞行信息', '检查航班信息']
-    # ['这里有什么有趣的？', '这里有什么有趣的', '这个地方有什么有趣的？', '这里有什么好玩的？', '这里有什么好玩的', '这个地方有什么好玩的？', '发生什么事了？', '这附近有什么好玩的地方吗', '有什么有趣的？', '附近有什么好玩的吗？', '这附近有什么好玩的', '附近有什麼好玩的', '附近有什么有趣的东西吗？']
+                cur_arr.extend(baidu_translate(tmp_q, 'zh'))
+        out_arr[query] = list(set(cur_arr))
+    return out_arr
 
+def write_translate2result(file_path='./data/input'):
+    with open(file_path, encoding='utf-8') as f, open(file_path + "_translate", 'w', encoding='utf-8') as out:
+        query_arr = [x.strip() for x in f.readlines()]
+        for k, v in back_translate(query_arr).items():
+            out.write("\t".join([k, ";".join(v)]) + "\n")
+
+
+
+if __name__ == '__main__':
+    write_translate2result()
+    # result:
+    # 帮我查一下航班信息	帮我查一下航班信息;请帮我查一下飞机的情报。;帮我检查航班信息。;检查我的航班信息。;检查航班;查一下我的飞行记录。;查一下我的航班信息。;检查一下飞行资料;检查飞行数据。;帮我查一下航班信息。;帮我查一下VOO信息;帮我查一下航班数据。;帮我查一下飞行记录。;请查一下飞机的信息。;幫我查一下班機資訊
+    # 查一下航班信息	打听一下航班的信息。;检查航班;检查VOO信息;查看航班信息;检查飞行数据。;四航班检查;检查航班信息。;航班信息验证;查一下班機資訊;请查一下飞机的信息。;检查飞行信息;检查航班信息
+    # 附近有什么好玩的	这里有什么有趣的？;这里有什么有趣的;这个地方有什么有趣的？;这里有什么好玩的？;这里有什么好玩的;这个地方有什么好玩的？;发生什么事了？;这附近有什么好玩的地方吗;有什么有趣的？;附近有什么好玩的吗？;这附近有什么好玩的;附近有什麼好玩的;附近有什么有趣的东西吗？
