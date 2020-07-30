ae8dcfc9a0e56fa890b8bf9a2d39633e25020391
zhiyang.zzy <993001803@qq.com>
2020-07-30 23:15:01 +0800

    modify augmentor

--- augmentor.py
+++ augmentor.py
@@ -5,15 +5,18 @@
 @Author  :   zhiyang.zzy 
 @Contact :   zhiyangchou@gmail.com
 @Desc    :   
+todo: 将各中增强方法在此汇合和使用，输入文件，输出为各中方法增强后的结果。
 '''
 
 # here put the import lib
-
-
-
-
-from eda import *
 import argparse
+from eda import *
+
+
+class Augmentor(object):
+    def __init__(self,):
+        pass
+
 ap = argparse.ArgumentParser()
 ap.add_argument("--input", required=True, type=str, help="input file of unaugmented data")
 ap.add_argument("--output", required=False, type=str, help="output file of unaugmented data")
